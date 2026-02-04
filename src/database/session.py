87c60422886e599e8f4cf7f1e2d2ee54d0ca321d
VYGAKOOL <vygakool@gmail.com>
2026-02-05 00:17:53 +0200

    return old settings

--- src/database/session.py
+++ src/database/session.py
@@ -5,14 +5,13 @@ from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession, async_sess
 from src.core import settings
 
 
-# POSTGRESQL_DATABASE_URL = (
-#     f"postgresql+asyncpg://{settings.POSTGRES_USER}:{settings.POSTGRES_PASSWORD}@"
-#     f"{settings.POSTGRES_HOST}:{settings.POSTGRES_PORT}/{settings.POSTGRES_DB}"
-# )
-POSTGRESQL_DATABASE_URL = "postgresql+asyncpg://user:password@localhost:5432/online_cinema_db"
+POSTGRESQL_DATABASE_URL = (
+    f"postgresql+asyncpg://{settings.POSTGRES_USER}:{settings.POSTGRES_PASSWORD}@"
+    f"{settings.POSTGRES_HOST}:{settings.POSTGRES_PORT}/{settings.POSTGRES_DB}"
+)
 
 engine = create_async_engine(
-    settings.DATABASE_URL,
+    POSTGRESQL_DATABASE_URL,
     echo=True,
     future=True,
     connect_args={"check_same_thread": False}
