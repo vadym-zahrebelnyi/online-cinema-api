cce8239761cc0d81dab6bc80089681e01e235003
VYGAKOOL <vygakool@gmail.com>
2026-02-06 18:25:23 +0200

    feature/add routes

--- src/main.py
+++ src/main.py
@@ -1,8 +1,8 @@
 from fastapi import FastAPI
+from src.orders.routes import router as orders_router
 
-app = FastAPI()
+from src.core.settings import settings
 
+app = FastAPI(title=settings.APP_NAME)
 
-@app.get("/")
-async def read_root():
-    return {"message": "Hello, World!"}
+app.include_router(orders_router, prefix=f"{settings.API_PREFIX}/orders", tags=["Orders"])
