fedd5d7eda970f77665ab39644bfdcf3b7a10e4b
Ostap Ohorodnyk <ostap.ohorodnyk@gmail.com>
2026-02-07 13:21:32 +0200

    feat: register movies router in main application

--- src/main.py
+++ src/main.py
@@ -1,8 +1,10 @@
 from fastapi import FastAPI
+from src.movies.router import router as movies_router
 
-app = FastAPI()
-
+app = FastAPI(title="Online Cinema API")
 
 @app.get("/")
 async def read_root():
     return {"message": "Hello, World!"}
+
+app.include_router(movies_router)
