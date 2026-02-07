2ce480ea049a40c4626ce5b64f6c06d4e99d2cb9
vadym-zahrebelnyi <zahrebelnyivadym@gmail.com>
2026-02-07 12:28:42 +0200

    refactor: add root router with versioned prefix

--- src/main.py
+++ src/main.py
@@ -1,12 +1,19 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, APIRouter
+
+from src.core.settings import settings
 
 from src.accounts.router import router as accounts_router
-from src.core.settings import settings
 from src.orders.routes import router as orders_router
 
 app = FastAPI(title=settings.APP_NAME)
 
-app.include_router(accounts_router, prefix="/accounts", tags=["Accounts"])
-app.include_router(
-    orders_router, prefix=f"{settings.API_PREFIX}/orders", tags=["Orders"]
-)
+api_router = APIRouter(prefix=settings.API_PREFIX)
+
+for router, prefix, tags in (
+        (accounts_router, "/accounts", ["Accounts"]),
+        (orders_router, "/orders", ["Orders"]),
+):
+    api_router.include_router(router, prefix=prefix, tags=tags)
+
+app.include_router(api_router)
+
