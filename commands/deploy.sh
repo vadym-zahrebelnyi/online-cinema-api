65076c60496ecb5ba8d2567824c26571d71f5a4a
vadym-zahrebelnyi <zahrebelnyivadym@gmail.com>
2026-02-08 17:38:42 +0200

    chore: separate prod and dev and finalize deploy

--- commands/deploy.sh
+++ commands/deploy.sh
@@ -2,6 +2,8 @@
 
 set -e
 
+DEPLOY_BRANCH="develop"
+
 handle_error() {
     echo "Error: $1"
     exit 1
@@ -10,14 +12,14 @@ handle_error() {
 cd /home/ubuntu/src/online-cinema-api || handle_error "Failed to navigate to the application directory."
 
 echo "Fetching the latest changes from the remote repository..."
-git fetch origin main || handle_error "Failed to fetch updates from the 'origin' remote."
+git fetch origin $DEPLOY_BRANCH  || handle_error "Failed to fetch updates from the 'origin' remote."
 
 echo "Resetting the local repository to match 'origin/main'..."
-git reset --hard origin/main || handle_error "Failed to reset the local repository to 'origin/main'."
+git reset --hard origin/$DEPLOY_BRANCH || handle_error "Failed to reset the local repository to 'origin/main'."
 
 echo "Fetching tags from the remote repository..."
 git fetch origin --tags || handle_error "Failed to fetch tags from the 'origin' remote."
 
-docker compose -f docker-compose.yml up -d --build || handle_error "Failed to build and run Docker containers using docker-compose.yml."
+docker compose -f docker-compose.prod.yml up -d --build || handle_error "Failed to build and run Docker containers using docker-compose.prod.yml."
 
 echo "Deployment completed successfully."
