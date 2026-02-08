d0984ead5b2cafc7f7ff6d6e67f9df2366426970
VYGAKOOL <vygakool@gmail.com>
2026-02-08 03:56:12 +0200

    refactor/change some endpoints

--- src/orders/routes.py
+++ src/orders/routes.py
@@ -3,13 +3,12 @@ from typing import Annotated, List
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from src.orders.crud import cancel_order
+from src.orders.crud import cancel_order, create_order
 from src.accounts.dependencies import get_current_user
 from src.accounts.models import UserDB
 from src.core.database import get_db
 from src.orders.crud import get_orders_by_user
-from src.orders.schemas import OrderItemCreateSchema, OrderReadSchema, CancelShema
-from src.orders.services import create_order_from_cart, get_order
+from src.orders.schemas import OrderReadSchema, CancelShema
 
 router = APIRouter()
 
@@ -28,24 +27,20 @@ async def get_my_orders_endpoint(
 
 @router.post("/", response_model=OrderReadSchema)
 async def create_order_endpoint(
-    user_id: int,
-    cart_items: List[OrderItemCreateSchema],
+    current_user: Annotated[UserDB, Depends(get_current_user)],
     db: Annotated[AsyncSession, Depends(get_db)],
 ):
     """
     Create an order from user's cart items
     """
     try:
-        order = await create_order_from_cart(
-            db, user_id, [item.model_dump() for item in cart_items]
-        )
-        return order
-    except ValueError as e:
+        return await create_order(db, current_user.id)
+    except Exception as e:
         raise HTTPException(status_code=400, detail=str(e))
 
 
 @router.patch("/{order_id}/cancel", response_model=CancelShema)
-async def pay_order_endpoint(
+async def cancel_order_endpoint(
     order_id: int, db: Annotated[AsyncSession, Depends(get_db)]
 ):
     """
