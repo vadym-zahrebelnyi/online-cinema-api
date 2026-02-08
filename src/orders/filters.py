19c1db53c34d4f42462d7de5d0a1a52aa0c059f8
VYGAKOOL <vygakool@gmail.com>
2026-02-08 20:42:06 +0200

    feature/implement filter

---
+++ src/orders/filters.py
@@ -0,0 +1,17 @@
+from datetime import datetime
+from fastapi_filter.contrib.sqlalchemy import Filter
+
+from .models import OrderDB, OrderStatusEnum
+
+
+class OrderFilter(Filter):
+    user_id: int | None = None
+    status: OrderStatusEnum | None = None
+
+    created_at__gte: datetime | None = None
+    created_at__lte: datetime | None = None
+
+    order_by: list[str] = ["-created_at"]
+
+    class Constants(Filter.Constants):
+        model = OrderDB
