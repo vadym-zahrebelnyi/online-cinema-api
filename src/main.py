9a1cde245ecc79b8fffb4a292437943416aeb448
Ostap Ohorodnyk <ostap.ohorodnyk@gmail.com>
2026-02-08 17:20:22 +0200

    added routes for the movie

--- src/main.py
+++ src/main.py
@@ -2,10 +2,10 @@ from fastapi import APIRouter, FastAPI
 
 from src.accounts.router import router as accounts_router
 from src.core.settings import settings
+from src.movies.router import router as movies_router
 from src.orders.routes import router as orders_router
 from src.payments.pages.router import router as payment_pages_router
 from src.payments.router import router as payments_router
-from src.movies.router import router as movies_router  # наш модуль
 
 app = FastAPI(title=settings.APP_NAME)
 
