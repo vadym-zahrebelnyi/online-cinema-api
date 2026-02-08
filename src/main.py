3200288b2bdd42d619cca37e7e784d69d41ead29
Oleksandr Prymak <prymak.olexandr@lll.kpi.ua>
2026-02-08 14:10:23 +0200

    chore: register cart router

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter, FastAPI
 
 from src.accounts.router import router as accounts_router
+from src.cart.router import router as cart_router
 from src.core.settings import settings
 from src.orders.routes import router as orders_router
 from src.payments.pages.router import router as payment_pages_router
@@ -12,6 +13,7 @@ api_router = APIRouter(prefix=settings.API_PREFIX)
 
 for router, prefix, tags in (
     (accounts_router, "/accounts", ["Accounts"]),
+    (cart_router, "/cart", ["Cart"]),
     (orders_router, "/orders", ["Orders"]),
     (payments_router, "/payments", ["Payments"]),
 ):
