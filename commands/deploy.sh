9878b88982ffd6118c4b99b032dd863ebbe786cd
vadym-zahrebelnyi <zahrebelnyivadym@gmail.com>
2026-02-08 13:45:58 +0200

    chore: base preparations and configurations for CD

---
+++ commands/deploy.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+set -e
+
+handle_error() {
+    echo "Error: $1"
+    exit 1
+}
+
+cd /home/ubuntu/src/online-cinema-api || handle_error "Failed to navigate to the application directory."
+
+echo "Fetching the latest changes from the remote repository..."
+git fetch origin main || handle_error "Failed to fetch updates from the 'origin' remote."
+
+echo "Resetting the local repository to match 'origin/main'..."
+git reset --hard origin/main || handle_error "Failed to reset the local repository to 'origin/main'."
+
+echo "Fetching tags from the remote repository..."
+git fetch origin --tags || handle_error "Failed to fetch tags from the 'origin' remote."
+
+docker compose -f docker-compose.yml up -d --build || handle_error "Failed to build and run Docker containers using docker-compose.yml."
+
+echo "Deployment completed successfully."
