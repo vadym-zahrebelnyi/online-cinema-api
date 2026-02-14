18a53bc651f613a01d6ffe923fbe6a1182e4a9f5
vadym-zahrebelnyi <zahrebelnyivadym@gmail.com>
2026-02-14 20:52:20 +0200

    test: fix test for order crud

--- src/orders/tests/test_crud.py
+++ src/orders/tests/test_crud.py
@@ -4,7 +4,6 @@ from unittest.mock import AsyncMock, MagicMock
 import pytest
 from sqlalchemy import select
 
-from src.cart.models import CartDB, CartItemDB
 from src.core import PaginationParams
 from src.orders.crud import (
     cancel_order,
@@ -120,15 +119,10 @@ async def test_create_order_cart_empty():
 
 @pytest.mark.asyncio
 async def test_create_order_success(monkeypatch):
-    """
-    Test successful creation of an order including DB interactions and cart processing.
-    """
     db = AsyncMock()
     cart_service = AsyncMock()
 
     class MockMovie:
-        """Mock movie object with id and price."""
-
         def __init__(self, id, price):
             self.id = id
             self.price = price
@@ -136,12 +130,13 @@ async def test_create_order_success(monkeypatch):
     movie1 = MockMovie(1, Decimal("10.5"))
     movie2 = MockMovie(2, Decimal("5.25"))
 
-    cart_item1 = CartItemDB()
+    cart_item1 = MagicMock()
     cart_item1.movie = movie1
-    cart_item2 = CartItemDB()
+
+    cart_item2 = MagicMock()
     cart_item2.movie = movie2
 
-    cart = CartDB()
+    cart = MagicMock()
     cart.items = [cart_item1, cart_item2]
 
     db.scalar = AsyncMock(
@@ -166,7 +161,10 @@ async def test_create_order_success(monkeypatch):
     db.flush = AsyncMock()
     db.commit = AsyncMock()
 
-    monkeypatch.setattr("src.orders.crud.create_order_items_from_cart", AsyncMock())
+    monkeypatch.setattr(
+        "src.orders.crud.create_order_items_from_cart",
+        AsyncMock()
+    )
 
     order = await create_order(db, user_id=1, cart_service=cart_service)
 
